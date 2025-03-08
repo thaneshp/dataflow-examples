80ad0f19448c91c38e47a4870ee5b93e94db71f1
Thanesh Pannirselvam <thaneshp@Thaneshs-MacBook-Air.local>
2025-03-09 09:42:43 +1100

    doc: updates readme

---
+++ terraform/gcp-resources/gcs.tf
@@ -0,0 +1,31 @@
+resource "google_storage_bucket" "project_bucket" {
+  name          = var.project_id
+  location      = var.region
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_object" "temp_folder" {
+  name          = "temp/"
+  content       = " "
+  bucket        = google_storage_bucket.project_bucket.name
+}
+
+resource "google_storage_bucket_object" "template_folder" {
+  name          = "template/"
+  content       = " "
+  bucket        = google_storage_bucket.project_bucket.name
+}
+
+resource "google_storage_bucket_object" "input_folder" {
+  name          = "input/"
+  content       = " "
+  bucket        = google_storage_bucket.project_bucket.name
+}
+
+resource "google_storage_bucket_object" "output_folder" {
+  name          = "output/"
+  content       = " "
+  bucket        = google_storage_bucket.project_bucket.name
+}
